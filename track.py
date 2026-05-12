07aa51e0891ad1983382d9c6de0398ed0e2df09a
UN64DD <azhar.janius2005@gmail.com>
2026-05-12 21:23:39 +0800

    updated

--- track.py
+++ track.py
@@ -1,54 +1,79 @@
-#import the necessary packages
 import numpy as np
 import argparse 
 import cv2
 
-# initialize the current frame of the video, along with the list of
-# ROI points along with whether or not this is input mode
 frame = None
 roiPts = []
 inputMode = False
 
 def selectROI(event, x, y, flags, param):
-    # grab the reference to the current frame, list of ROI
-    # points and whether or not it is ROI selection mode
     global frame, roiPts, inputMode
 
-    # if we are in ROI selection mode, the mouse was clicked
-    # and we do not already have four points, then update the
-    # list of ROI points with the (x, y) location of the click
-    # and draw the circle
-    if inputMode and event == cv2.EVENT_LBUTTONDOWN and len(roiPits) < 4:
-        roiPits.append((x, y))
-        cv2.circle(frame, (x,y), 4, (0, 255, 0), 2)
+    if inputMode and event == cv2.EVENT_LBUTTONDOWN and len(roiPts) < 4:
+        roiPts.append((x, y))
+        cv2.circle(frame, (x, y), 4, (0, 255, 0), 2)
         cv2.imshow("frame", frame)
  
 def main():
-    # construct the argument parse and parse the arguments
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-v", "--video", 
-                    help = "path to the optional video file")
-    args = vars(ap.parse_args())
-
-    # grab the reference to the current frame, list of ROI
-    # points and whether or not it is ROI selection mode
     global frame, roiPts, inputMode
 
-    # if the video path was not supplied, grab the reference to the
-    # camera
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-v", "--video", help="path to the optional video file")
+    args = vars(ap.parse_args())
+
     if not args.get("video", False):
         camera = cv2.VideoCapture(0)
-
-    # otherwise, load the video
     else:
         camera = cv2.VideoCapture(args["video"])
 
-    # setup the mouse callback
     cv2.namedWindow("frame")
     cv2.setMouseCallback("frame", selectROI)
 
-    # initialize the termination criteria for cam shift, indicating
-    # a maximum of ten iterations or movement by at least one pixel
-    # along with the bounding box of the ROI
     termination = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
     roiBox = None
+    roiHist = None
+
+    while True:
+        (grabbed, frame) = camera.read()
+
+        if not grabbed:
+            break
+
+        if roiBox is not None:
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            backProj = cv2.calcBackProject([hsv], [0], roiHist, [0, 180], 1)
+            (r, roiBox) = cv2.CamShift(backProj, roiBox, termination)
+            pts = np.int0(cv2.boxPoints(r))
+            cv2.polylines(frame, [pts], True, (0, 255, 0), 2)
+
+        cv2.imshow("frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == ord('i') and len(roiPts) < 4:
+            inputMode = True
+            orig = frame.copy()
+
+            while len(roiPts) < 4:
+                cv2.imshow("frame", frame)
+                cv2.waitKey(0)
+
+            roiPts = np.array(roiPts)
+            s = roiPts.sum(axis=1)
+            tl = roiPts[np.argmin(s)]
+            br = roiPts[np.argmax(s)]
+
+            roi = orig[tl[1]:br[1], tl[0]:br[0]]
+            roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+
+            roiHist = cv2.calcHist([roi], [0], None, [16], [0, 180])
+            roiHist = cv2.normalize(roiHist, roiHist, 0, 255, cv2.NORM_MINMAX)
+            roiBox = (tl[0], tl[1], br[0], br[1])
+
+        elif key == ord("q"):
+            break
+
+    camera.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
